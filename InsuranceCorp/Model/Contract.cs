6e1e452a33bb3b5cf6416f3df992e386f2df0a82
kubicek-skoleni <kubicek.brno@gmail.com>
2023-10-11 13:20:37 +0200

    Ins Corp

---
+++ InsuranceCorp/Model/Contract.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Reflection.Emit;
+
+namespace InsuranceCorp.Model;
+public class Contract
+{
+    public int Id { get; set; }
+
+    [MaxLength(250)]
+    public string? Name { get; set; }
+
+    [MaxLength(250)]
+    public string? Number { get; set; }
+    public DateTime Signed { get; set; }
+    public bool IsActive { get; set; }
+    public override string ToString() => $"{Number} {Name} {IsActive} {Signed.ToString("yyyy-MM-dd")}";
+
+}
---
+++ InsuranceCorp/Model/Person.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Reflection.Emit;
+using System.Runtime.Loader;
+
+namespace InsuranceCorp.Model;
+
+public class Person
+{
+    public int Id { get; set; }
+
+    [MaxLength(250)]
+    public string? FirstName { get; set; }
+    [MaxLength(250)]
+
+    public string? LastName { get; set; }
+
+    [MaxLength(525)]
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+    public Address? Address { get; set; }
+    public ICollection<Contract> Contracts { get; set; } = new HashSet<Contract>();
+    public override string ToString() => $"{FirstName} {LastName} {Email} {DateOfBirth.ToString("yyyy")} ({Contracts?.Count()}) {Address?.City}";
+
+}
